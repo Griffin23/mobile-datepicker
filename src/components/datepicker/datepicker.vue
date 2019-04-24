521971255d4f537b784beec7e3e109502a6d8b04
Vellanueva <838884524@qq.com>
2019-04-24 21:37:35 +0800

    feat: close图标样式

--- src/components/datepicker/datepicker.vue
+++ src/components/datepicker/datepicker.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-        hello datepicker
+    <div class="datepicker-container">
+        <div class="datepicker-close-container">
+            <img src="../../assets/img/icon-close.png" alt="close">
+        </div>
         <button @click="selectDate">select {{ selectedDate }}</button>
     </div>
 </template>
@@ -25,6 +27,30 @@
     }
 </script>
 
-<style>
-
-</style>
+<style lang="scss" scoped>
+    @import "../../assets/css/reset.css";
+    .datepicker-container {
+        position: fixed;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        z-index: 1000;
+        overflow-y: scroll;
+        -webkit-overflow-scrolling: touch;
+        background: #fff;
+    }
+    $close-vertical-margin: 16px;
+    $close-width: 22px;
+    .datepicker-close-container {
+        height: $close-width + 2 * $close-vertical-margin
+    }
+    .datepicker-close-container img {
+        position: absolute;
+        top: 0;
+        right: 0;
+        width: $close-width;
+        height: $close-width;
+        margin-top: $close-vertical-margin;
+        margin-right: $close-vertical-margin;
+    }
+</styles>
