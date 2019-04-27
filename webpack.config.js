e442030f736ae0f281a01f91dc3e86214042ecbc
Vellanueva <838884524@qq.com>
2019-04-27 16:19:58 +0800

    feat: 配置 npm发布

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,15 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  entry: './src/main.js',
+  entry: NODE_ENV === 'development' ? './src/main.js' : './src/index.js',
   output: {
+    // 修改打包出口，在最外级目录打包出一个 index.js 文件，我们 import 默认会指向这个文件
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: 'build.js'
+    filename: 'mobile-datepicker.js',
+    library: 'mobile-datepicker', // 指定的就是你使用require时的模块名
+    libraryTarget: 'umd', // libraryTarget会生成不同umd的代码,可以只是commonjs标准的，也可以是指amd标准的，也可以只是通过script标签引入的
+    umdNamedDefine: true // 会对 UMD 的构建过程中的 AMD 模块进行命名。否则就使用匿名的 define
   },
   module: {
     rules: [
@@ -96,7 +100,7 @@ if (process.env.NODE_ENV === 'production') {
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
+      sourceMap: false,
       compress: {
         warnings: false
       }
