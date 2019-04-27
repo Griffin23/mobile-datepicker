3ebd234a39a948cfe9b9e699903e2e611d2bd10e
Vellanueva <838884524@qq.com>
2019-04-27 16:32:09 +0800

    feat: 修改package name

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,8 @@ module.exports = {
     // 修改打包出口，在最外级目录打包出一个 index.js 文件，我们 import 默认会指向这个文件
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: 'mobile-datepicker.js',
-    library: 'mobile-datepicker', // 指定的就是你使用require时的模块名
+    filename: 'mobile-fill-screen-datepicker.js',
+    library: 'mobile-fill-screen-datepicker', // 指定的就是你使用require时的模块名
     libraryTarget: 'umd', // libraryTarget会生成不同umd的代码,可以只是commonjs标准的，也可以是指amd标准的，也可以只是通过script标签引入的
     umdNamedDefine: true // 会对 UMD 的构建过程中的 AMD 模块进行命名。否则就使用匿名的 define
   },
